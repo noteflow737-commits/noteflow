92623d9da161eaa2bbd507292a138c48a0c92b1c
noteflow737-commits <noteflow737@gmail.com>
2026-06-12 05:16:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,2 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAM8zmBFYHixjn6mF8Sdr98Bz-pZruoZpo",
-  authDomain: "noteflow-9aa06.firebaseapp.com",
-  projectId: "noteflow-9aa06",
-  storageBucket: "noteflow-9aa06.firebasestorage.app",
-  messagingSenderId: "459942250903",
-  appId: "1:459942250903:web:46036bf79f951f1ca91cd2",
-  measurementId: "G-532ZZEWX8T"
-};
-
-const app = initializeApp(firebaseConfig);
-
-console.log("Firebase connected");
+alert("script.js is running");
+console.log("script.js is running");
