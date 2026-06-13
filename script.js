3c3e00b6b5505cf60e2a8ba816e4994095868526
noteflow737-commits <noteflow737@gmail.com>
2026-06-13 22:45:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-console.log("script loaded");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyAM8zmBFYHixjn6mF8Sdr98Bz-pZruoZpo",
+authDomain: "noteflow-9aa06.firebaseapp.com",
+projectId: "noteflow-9aa06",
+storageBucket: "noteflow-9aa06.firebasestorage.app",
+messagingSenderId: "459942250903",
+appId: "1:459942250903:web:46036bf79f951f1ca91cd2"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+console.log("Firebase connected");
 
 document.getElementById("loginBtn").addEventListener("click", () => {
-alert("Button works");
+alert("Login button clicked");
 });
